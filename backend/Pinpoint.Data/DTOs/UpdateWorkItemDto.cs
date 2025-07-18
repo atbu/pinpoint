c1fca11c228edbab4500edb12884541c1d9a2e62
Alex <186313861+atbu@users.noreply.github.com>
2025-07-18 19:31:52 +0100

    Add Status property to WorkItem model and represent this on the frontend as text by converting the enum

--- backend/Pinpoint.Data/DTOs/UpdateWorkItemDto.cs
+++ backend/Pinpoint.Data/DTOs/UpdateWorkItemDto.cs
@@ -1,7 +1,10 @@
+using Pinpoint.Data.Models;
+
 namespace Pinpoint.Data.DTOs;
 
 public class UpdateWorkItemDto
 {
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public Status Status { get; set; } = Status.New;
 }
