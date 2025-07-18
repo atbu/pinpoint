8a0b3b7ccbb625cf4f97c8a8e70421151d13476f
Alex <186313861+atbu@users.noreply.github.com>
2025-07-18 18:18:45 +0100

    Update CORS configuration

--- backend/Pinpoint.API/Program.cs
+++ backend/Pinpoint.API/Program.cs
@@ -15,6 +15,18 @@ builder.Services.InjectDependencies();
 builder.Services.AddDbContextPool<Context>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("Pinpoint")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAllPolicy",
+        policy =>
+        {
+            // TODO: this is only for development and SHOULD NOT exist when deployed
+            policy.AllowAnyOrigin();
+            policy.AllowAnyHeader();
+            policy.AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,6 +38,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("AllowAllPolicy");
 
 app.MapControllers();
 
