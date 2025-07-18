8c2c3d9b7da7ff417918ce880ebd5a04da817ff4
Alex <186313861+atbu@users.noreply.github.com>
2025-07-18 19:36:08 +0100

    Add status property to workitem (#5)

---
+++ backend/Pinpoint.Data/Migrations/20250718182107_AddStatusColumnToWorkItem.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pinpoint.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusColumnToWorkItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "WorkItems",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "WorkItems");
+        }
+    }
+}
